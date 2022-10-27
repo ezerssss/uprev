b31d97663d0271f213d9ecd5eaee03636c2637e4
Ezra Magbanua <magbanuaezra@gmail.com>
2022-10-27 23:38:01 +0800

    toggle maple

--- src/pages/FlashcardsPage/components/Flashcard.tsx
+++ src/pages/FlashcardsPage/components/Flashcard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useRef } from 'react';
 import { shuffle } from '../../../helpers/shuffle';
 import { CardsInterface } from '../../../interfaces/flashcards';
 
@@ -11,13 +11,27 @@ function Flashcard(props: PropsInterface) {
     const { keyword, description } = card;
     const randomlyArrangedText = shuffle([keyword, description]);
 
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    function handleClick() {
+        if (!ref.current) return;
+
+        ref.current.classList.toggle('flipped-card');
+    }
+
     return (
         <div className="flip-card w-full lg:w-1/2 min-h-[25rem] m-auto pt-7">
-            <div className="flip-card-inner">
-                <div className="flip-card-front min-h-[20rem] border rounded-2xl drop-shadow cursor-pointer p-6 flex items-center justify-center">
+            <div className="flip-card-inner" ref={ref}>
+                <div
+                    className="flip-card-front min-h-[20rem] border rounded-2xl drop-shadow cursor-pointer p-6 flex items-center justify-center"
+                    onClick={handleClick}
+                >
                     <p>{randomlyArrangedText[0]}</p>
                 </div>
-                <div className="flip-card-back min-h-[20rem] border rounded-2xl drop-shadow cursor-pointer p-6 flex items-center justify-center">
+                <div
+                    className="flip-card-back min-h-[20rem] border rounded-2xl drop-shadow cursor-pointer p-6 flex items-center justify-center"
+                    onClick={handleClick}
+                >
                     <p>{randomlyArrangedText[1]}</p>
                 </div>
             </div>
